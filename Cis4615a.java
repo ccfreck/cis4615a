b88c022412cb2d0e631570950f28cb2e5ab496d7
ccfreck <cafreckmann@calebPC>
2024-10-04 14:12:46 -0400

    From Caleb Freckmann change

--- Cis4615a.java
+++ Cis4615a.java
@@ -13,11 +13,13 @@
  *
  ******************************************************************************/
 
-public class Cis4615a {
+public class Cis4615a 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World - from the class of CIS4615 - Fall 2022 - UCF");
+        System.out.println("Hello, World - from Caleb Freckmann");
     }
 
 }
