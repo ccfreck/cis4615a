d7ee3da32530dc1159e8467a6584ad71007a1b1f
ccfreck <cafreckmann@calebPC>
2024-10-05 13:17:30 -0400

    Rule 05 Compliant Code

--- R05_OBJ11_J.java
+++ R05_OBJ11_J.java
@@ -4,12 +4,12 @@ public class R05_OBJ11_J
 {
     public static void main(String[] args)
     {
-        // An attacker can replace the function table as follows:
-        // FunctionTable.m_functions = new_table;
+        // Now, n attacker cant replace the function table as follows due to the final keyword that treats it as a constant:
+        // FunctionTable.m_functions = new_table; XXXXX
     }
 }
 
 class FunctionTable
 {
-    public static FuncLoader m_functions;
+    public static final FuncLoader m_functions;
 }
