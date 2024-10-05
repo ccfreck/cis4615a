97b5890f916e073151d6d0b4764c42faadbc190d
ccfreck <cafreckmann@calebPC>
2024-10-05 13:25:51 -0400

    Rule 09 LCK Compliant Code

--- R09_LCK01_J.java
+++ R09_LCK01_J.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class R09_LCK01_J
 {
-    private final String lock = "LOCK";
+    private final String lock = new String("LOCK");
 
     public static void main(String[] args)
     {
