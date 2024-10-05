e172f8103a98642ee5b5f4ab3598bc3a8e163fb2
ccfreck <cafreckmann@calebPC>
2024-10-04 22:24:34 -0400

    Rule 00. Input Validation and Data Sanitization (IDS)

---
+++ R00_IDS03_J.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class R00_IDS03_J
+{
+    public static void main(String[] args)
+    {
+        if (loginSuccessful)
+        {
+            logger.severe("User login succeeded for: " + username);
+        }
+        else
+        {
+            logger.severe("User login failed for: " + username);
+        }
+    }
+}
