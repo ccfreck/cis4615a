7461b019462dad54290063a9cbc66a33b8a6662d
ccfreck <cafreckmann@calebPC>
2024-10-05 13:24:58 -0400

    Rule 09. LCK Do no tsychronize on objects that may be reused.

---
+++ R09_LCK01_J.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class R09_LCK01_J
+{
+    private final String lock = "LOCK";
+
+    public static void main(String[] args)
+    {
+        synchronized(lock)
+        {
+            // ...
+        }
+    }
+}
