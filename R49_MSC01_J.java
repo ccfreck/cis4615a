02e07ee139ce48031843858378aad4f2afbce454
ccfreck <cafreckmann@calebPC>
2024-10-05 14:03:04 -0400

    Rule 49 MSC01 Do not use an empty infinite loop Non compliant code

---
+++ R49_MSC01_J.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class R49_MSC01_J
+{
+    public static void main(String[] args)
+    {
+        // nop();
+    }
+
+    public int nop()
+    {
+        while (true) {}
+    }
+}
