f331d4d85b2e0b0eeff7e018d1df78fe4730e6d9
ccfreck <cafreckmann@calebPC>
2024-10-04 23:03:27 -0400

    Rule 03 NUM Non-compliant Code

---
+++ R03_NUM03_J.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class R03_NUM03_J
+{
+    public static void main(String[] args)
+    {
+        // getInteger();
+    }
+
+    public static int getInteger(DataInputStream is) throws IOException
+    {
+        return is.readInt();
+    }
+}
