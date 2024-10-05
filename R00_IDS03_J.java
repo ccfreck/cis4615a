df732f5644c3044a96c761f1556bdc2a8b2e5144
ccfreck <cafreckmann@calebPC>
2024-10-04 22:31:49 -0400

    Compliant Solution Rule 00. IDS

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -2,15 +2,26 @@ import java.util.*;
 
 public class R00_IDS03_J
 {
+    private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+
     public static void main(String[] args)
     {
+        // conditions to check code
+        boolean loginSuccessful = true;
+        String username = "user123";
+        
         if (loginSuccessful)
         {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
         }
         else
         {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitizeUser(username));
         }
+    }
+
+    public static String sanitizeUser(String username)
+    {
+        return Pattern.matches("[A-Za-z0-9]+", username) ? username : "unauthorized user";
     }
 }
