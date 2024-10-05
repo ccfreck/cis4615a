dbf39b728dba47377e0a1b19bcc64d36248943d2
ccfreck <cafreckmann@calebPC>
2024-10-04 22:42:55 -0400

    EXP non-compliant

---
+++ R02_XP00_J.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class R02_XP00_J
+{
+    public static void main(String [] args)
+    {
+        // deleteFile();
+    }
+    public void deleteFile()
+    {
+        File someFile = new File("someFileName.txt");
+        // Do something with someFile
+        someFile.delete();
+    }
+}
