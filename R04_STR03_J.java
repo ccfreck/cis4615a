2fb3978af3615c9ba45bb62f37d0cf948c348788
ccfreck <cafreckmann@calebPC>
2024-10-04 23:13:00 -0400

    Rule 04 STR Compliant Code

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -5,9 +5,9 @@ public class R04_STR03_J
     public static void main(String[] args)
     {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);   
+        String s = x.toString(); // valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns); 
     }
 }
