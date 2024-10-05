00c35ca9f7b33466eccb71cbf16a522d3e2d92f9
ccfreck <cafreckmann@calebPC>
2024-10-05 13:16:05 -0400

    Rule 05 OBJ 11 Non Compliant Code - Do not use public static non-final fields

---
+++ R05_OBJ11_J.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class R05_OBJ11_J
+{
+    public static void main(String[] args)
+    {
+        // An attacker can replace the function table as follows:
+        // FunctionTable.m_functions = new_table;
+    }
+}
+
+class FunctionTable
+{
+    public static FuncLoader m_functions;
+}
--- R05_OBJ_J.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class R05_OBJ_J
-{
-    public static void main(String[] args)
-    {
-        public static int counter = 0;
-    }
-}
